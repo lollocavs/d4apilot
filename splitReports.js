4f3a2566adef0d05f34b79a4a2bc4d76fcce0fbd
lollocavs <lorenzocavalieri@me.com>
2015-11-20 12:44:05 +0100

    Update package.json

--- splitReports.js
+++ splitReports.js
@@ -3,13 +3,15 @@
 var jf = require('jsonfile');
 var jfw = require('jsonfile');
 var util = require('util');
-var file = '/Users/lollo/Desktop/Reports/reportsCollection.json';
-var folder = '/Users/lollo/Desktop/Reports/';
+var file = '/Users/d4a-admin/Desktop/Reports/reportsCollection.json';
+var folder = '/Users/d4a-admin/Desktop/Reports/';
 
 jf.readFile(file, function(err, obj) {
-    console.log('Reports letto!');
-    console.log(obj);
-    
+    if (err) throw err;
+    else {
+        console.log('Reports letto!');
+        console.log(obj);
+    }
     obj.forEach(function(element){
         var filename = folder + element.name + '.json';
         jfw.writeFile(filename,element,function(err){
