77046b0af6e81486d8374adccc78a591867140c8
Lorenzo Cavalieri <lorenzocavalieri@me.com>
2019-01-08 08:34:33 +0100

    Added morgan middleware for server logging

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-
 //Configuration
 var serverExpress = 'http://localhost:3000';
-var mongoDB = 'mongodb://localhost:27017/mongod4a';
+var mongoDB = 'mongodb://mongo:27017/mongod4a';
 
 // Dependencies
 var express = require('express');
+var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 
 // MongoDB
-mongoose.connect(mongoDB, function(err,res){
-    if(err) throw err;
+mongoose.connect(mongoDB, { useNewUrlParser: true }, function(err, res) {
+    if (err) throw err;
     console.log('Connessione stabilita');
 });
 
@@ -19,6 +19,7 @@ mongoose.connect(mongoDB, function(err,res){
 // Express
 var app = express();
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use('/', express.static(__dirname + '/www/'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -29,4 +30,3 @@ app.use('/api', require('./routes/api'));
 // Start server
 app.listen(3000);
 console.log('API is running on port 3000');
-
