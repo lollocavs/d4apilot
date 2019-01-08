58b64d3991f550a23635a11351f2585d143bf5c5
Lorenzo Cavalieri <lorenzocavalieri@me.com>
2019-01-08 13:04:34 +0100

    Improved script for splitting reports

--- splitReports.js
+++ splitReports.js
@@ -2,21 +2,24 @@
 
 var jf = require('jsonfile');
 var jfw = require('jsonfile');
+var path = require('path');
 var util = require('util');
-var file = '/Users/d4a-admin/Desktop/Reports/reportsCollection.json';
-var folder = '/Users/d4a-admin/Desktop/Reports/';
-
-jf.readFile(file, function(err, obj) {
+var file = path.join(__dirname, 'reports.json');
+console.log('[ Target file] : ', file);
+var folder = path.join(__dirname, 'reports');
+console.log('[ Target folder] : ', folder);
+jf.readFile(file, (err, obj) => {
     if (err) throw err;
     else {
-        console.log('Reports letto!');
-        console.log(obj);
+        console.log('File di esportazione letto!');
+        // console.log(obj);
     }
-    obj.forEach(function(element){
-        var filename = folder + element.name + '.json';
-        jfw.writeFile(filename,element,function(err){
+    obj.forEach(function(element) {
+        var filename = path.join(folder, element.name + '.json');
+        jfw.writeFile(filename, element, function(err) {
             if (err) throw err;
+            console.log('[ File creato] : ', element.name + '.json');
         });
-    
+
     });
 })
