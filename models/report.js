a8f1b10e105c8e4c7cf399eb441c73e82417fa73
Lorenzo Cavalieri <lorenzocavalieri@me.com>
2019-01-09 12:30:13 +0100

    Update report model

--- models/report.js
+++ models/report.js
@@ -1,27 +1,25 @@
-
 // Dependencies
 var restful = require('node-restful');
 var mongoose = restful.mongoose;
 
 // Schema
 var reportSchema = new mongoose.Schema({
-//    _id: Schema.ObjectId,
-    name : String,
-    appReports : [
-        {   
-            'userID' : String,
-            'id' : Number,
-            'appName': String,
-            'stepNumber': Number,
-            'Tstart': Number,
-            'DTmax': Number,
-            'validTaps': [],
-            'invalidTaps': [],
-            'Tsuccess': [],
-            'Success': [],
-            'setStartTime': Number
-        }
-    ]
+    //    _id: Schema.ObjectId,
+    origin: String,
+    name: String,
+    appReports: [{
+        'userID': String,
+        'id': Number,
+        'appName': String,
+        'stepNumber': Number,
+        'Tstart': Number,
+        'DTmax': Number,
+        'validTaps': [],
+        'invalidTaps': [],
+        'Tsuccess': [],
+        'Success': [],
+        'setStartTime': Number
+    }]
 });
 
 // Return model
