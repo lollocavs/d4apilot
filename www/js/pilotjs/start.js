1d31c6ad76c74ce738abcdbb5bbfcd5b60f56817
Lorenzo Cavalieri <lorenzocavalieri@me.com>
2019-01-09 19:05:02 +0100

    Reset User Counter

--- www/js/pilotjs/start.js
+++ www/js/pilotjs/start.js
@@ -1,15 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function() {
     if (!localStorage.userCounter || localStorage.userCounter.isNaN) {
-        localStorage.userCounter = 12;
+        localStorage.userCounter = 1;
     }
-    
-    $('#start').on('click',function(e){
-        var ctr = parseInt(localStorage.userCounter,10);
+
+    $('#start').on('click', function(e) {
+        var ctr = parseInt(localStorage.userCounter, 10);
         ctr++;
         localStorage.userCounter = ctr;
         var reportName = 'report' + localStorage.userCounter;
         var report = new reportManager(reportName);
-        localStorage.setItem(reportName,JSON.stringify(report));
+        localStorage.setItem(reportName, JSON.stringify(report));
     });
 });
-
