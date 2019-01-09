25368051159dada25613e30faadbe879a97e5a0a
lollocavs <lorenzocavalieri@me.com>
2019-01-09 13:54:17 +0000

    Disabled config test file

--- www/js/pilotjs/appManager.js
+++ www/js/pilotjs/appManager.js
@@ -20,8 +20,8 @@ function getTitlePage(appCounter, totalApp) {
 
 function getAppConfig(appName) {
     //! TEST CONFIGURATION
-    var url = './config/appConfig_test.json';
-    // var url = './config/appConfig.json';
+    // var url = './config/appConfig_test.json';
+    var url = './config/appConfig.json';
     var request = $.ajax({
         type: 'GET',
         url: url,
